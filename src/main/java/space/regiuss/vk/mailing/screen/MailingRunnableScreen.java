d87fe6193498f4b71155ce0215b2ae722b4810aa
regiuss <regiuslink@gmail.com>
2024-01-21 16:03:49 +0300

    mailing screen add export

--- src/main/java/space/regiuss/vk/mailing/screen/MailingRunnableScreen.java
+++ src/main/java/space/regiuss/vk/mailing/screen/MailingRunnableScreen.java
@@ -11,6 +11,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.util.Duration;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -35,9 +36,11 @@ import space.regiuss.vk.mailing.task.MailingTask;
 
 import javax.annotation.PostConstruct;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Log4j2
@@ -233,6 +236,55 @@ public class MailingRunnableScreen extends RunnablePane {
     private void start() {
         startButton.setDisable(true);
         stopButton.setDisable(false);
+    }
+
+    @FXML
+    public void onExportClick(ActionEvent event) {
+        if (kitListView.getItems().isEmpty()) {
+            app.showAlert(new SimpleAlert("Текущий набор пуст", AlertVariant.WARN), Duration.seconds(5));
+            return;
+        }
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("csv", "*.csv"));
+        File file = chooser.showSaveDialog(app.getStage());
+        if (file == null)
+            return;
+        Button button = (Button) event.getTarget();
+        button.setDisable(true);
+        app.showAlert(new SimpleAlert("Начинаю экспорт набора", AlertVariant.SUCCESS), Duration.seconds(5));
+        CompletableFuture<Void> completableFuture = new CompletableFuture<>();
+        completableFuture.whenComplete((r, e) -> Platform.runLater(() -> {
+            button.setDisable(false);
+            if (e == null) {
+                app.showAlert(new SimpleAlert("Экспорт набора завершен", AlertVariant.SUCCESS), Duration.seconds(5));
+            } else {
+                log.warn("export error", e);
+                app.showAlert(new SimpleAlert("Не удалось экспортировать набор: " + e.getMessage(), AlertVariant.DANGER), Duration.seconds(5));
+            }
+        }));
+        app.getExecutorService().execute(() -> {
+            try (OutputStream os = new FileOutputStream(file)) {
+                os.write(239);
+                os.write(187);
+                os.write(191);
+                os.write("ссылка;id;тип;имя;подписчики;фото\n".getBytes(StandardCharsets.UTF_8));
+                for (ImageItemWrapper<Page> item : kitListView.getItems()) {
+                    Page page = item.getItem();
+                    os.write(String.format(
+                            "%s;%s;%s;%s;%s;%s%n",
+                            page.getLink(),
+                            page.getId(),
+                            page.getType().name(),
+                            page.getName().replace(";", ""),
+                            page.getSubscribers(),
+                            page.getIcon()
+                    ).getBytes(StandardCharsets.UTF_8));
+                }
+                completableFuture.complete(null);
+            } catch (Exception e) {
+                completableFuture.completeExceptionally(e);
+            }
+        });
     }
 
     @FXML
